3eeef91425e91075704b92075858c001a1ab3d0d
marciano-web <marcianofmarques@hotmail.com>
2026-01-14 01:48:51 -0300

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from .config import settings
+from .config import get_settings
+
+settings = get_settings()
+
 
 engine = create_engine(settings.database_url, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
